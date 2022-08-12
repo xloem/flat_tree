35e2654f4d6f67e09ceae8d2962c3c77295af191
Karl Semich <0xloem@gmail.com>
2022-08-12 10:52:50 -0400

    test.py for append_indices

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-from mix_indices import append_indices
+#from mix_indices import append_indices
+import append_indices
 
-index = append_indices(degree=3)
+index = append_indices.append_indices(degree=3)
 stored_indices = {}
 
 import random
@@ -9,14 +10,18 @@ data = [random.randbytes(random.randint(1,8)) for x in range(24)]
 for chunk in data:
     id = index.leaf_count
     stored_indices[id] = (index.copy(), chunk)
-    index.append(id, len(chunk))
+    index.append(len(chunk), id)
 
 def iterate(index, start_offset, end_offset):
     subendoffset = 0
-    for subleafcount, suboffset, subsize, subid in index:
+    for subleafcount, *_, subsize, subid in index:
         subindex, subdata = stored_indices[subid]
+        substartoffset = subendoffset
         subendoffset += subsize
         if subendoffset > start_offset:
+            #adjusted_start = start_offset - substartoffset + suboffset
+            #adjusted_end = end_offset - substartoffset + suboffset
+            #data = b''.join(iterate(subindex, adjusted_start, min(adjusted_end, subsize - adjusted_start)))
             data = b''.join(iterate(subindex, start_offset, end_offset))
             yield data
             if subendoffset > end_offset:
