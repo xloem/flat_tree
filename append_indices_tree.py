3df455f5b18811a86a9f403d474096d415b59799
Karl Semich <0xloem@gmail.com>
2022-08-12 04:44:59 -0400

    stable append with size

--- append_indices_tree.py
+++ append_indices_tree.py
@@ -1,6 +1,8 @@
 # indexes a balanced tree of past indices
-# publish paired with every write to retain a lookup
+# every write, publish a .copy() paired with data, then pass the locator to .append() to index it
+
 class append_indices(list):
+
     def __init__(self, degree = 2, initial_indices = []):
         super().__init__(*initial_indices)
         self.degree = degree
@@ -8,17 +10,20 @@ class append_indices(list):
         for leaf_count, size, value in self:
             self.leaf_count += leaf_count
             self.size += size
-    def append(self, last_id, size):
+
+    def append(self, added_size, last_publish):
         self.leaf_count += 1
-        self.size += size
+        self.size += added_size
+
         new_leaf_count = self.leaf_count
         new_size = self.size
-        idx = -1
+
         for idx, (branch_leaf_count, branch_size, branch_id) in enumerate(self):
             if branch_leaf_count * self.degree <= new_leaf_count:
                 break
             new_leaf_count -= branch_leaf_count
             new_size -= branch_size
         else:
-            idx += 1
-        self[idx:] = [(new_leaf_count, new_size, last_id)]
+            idx = len(self)
+
+        self[idx:] = [(new_leaf_count, new_size, last_publish)]
--- test.py
+++ test.py
@@ -1,19 +1,20 @@
 from append_indices_tree import append_indices
 
-index = append_indices()
+index = append_indices(degree=3)
 stored_indices = {}
 
 import random
-data = [random.random() for x in range(16)]
+data = [random.random() for x in range(24)]
 for offset, new_data in enumerate(data):
     id = index.leaf_count
-    stored_indices[id] = ([*index], offset + 1, new_data)
+    stored_indices[id] = (index.copy(), new_data)
     index.append(id, 1)
 
 def iterate(index, start_offset, end_offset):
-    offset = 0
+    subendoffset = 0
     for subleafcount, subsize, subid in index:
-        subindex, subendoffset, subdata = stored_indices[subid]
+        subindex, subdata = stored_indices[subid]
+        subendoffset += subsize
         if subendoffset > start_offset:
             yield from iterate(subindex, start_offset, end_offset)
             if subendoffset > end_offset:
@@ -22,6 +23,9 @@ def iterate(index, start_offset, end_offset):
             start_offset = subendoffset
 
 print(data)
-cmp = [*iterate(index, 0, offset + 1)]
+cmp = [*iterate(index, 0, index.size)]
 print(cmp)
+
 assert data == cmp
+
+print(index)
